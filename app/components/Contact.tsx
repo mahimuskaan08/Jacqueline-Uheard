dff347e34810eecb19357670a1a37901e09ef750
mahimuskaan08 <mcube3digitals@gmail.com>
2026-06-30 04:31:31 -0400

    Fix Google Maps embed: add allowFullScreen and clean URL

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -92,7 +92,7 @@ export default function Contact() {
           >
             <iframe
               title="U-Heard Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3021.265258!2d-74.2182!3d40.7676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c3ae97b6b5c4f3%3A0x0!2s475+William+St%2C+East+Orange%2C+NJ+07017!5e0!3m2!1sen!2sus!4v1713700000000!5m2!1sen!2sus"
+              src="https://www.google.com/maps?q=475+William+St,+East+Orange,+NJ+07017&output=embed"
               width="100%"
               height="100%"
               style={{
@@ -103,6 +103,7 @@ export default function Contact() {
                 width: '100%',
                 height: '100%',
               }}
+              allowFullScreen
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
             />
